2a865fdf64d61043b4c8ae7f54cead1ab1b7011e
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-11-12 10:36:21 +1300

    Version bump

--- version.php
+++ version.php
@@ -15,4 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2010070500;
+$plugin->version   = 2012111200;
+$plugin->release   = "1.0.1";
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->requires  = 2012062500;
