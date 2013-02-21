9502f1bdaa4fdbcd44dbc4832d6d8fb283960c99
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-02-21 14:27:35 +1300

    Add "addinstance" capability

--- db/access.php
+++ db/access.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,7 +14,25 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version   = 2013011702;
-$plugin->release   = "1.1.1";
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->requires  = 2012062500;
+/**
+ * menu_site_and_course block caps.
+ *
+ * @package    block_menu_site_and_course
+ * @copyright  Paul Nicholls, 2013
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'block/menu_site_and_course:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
